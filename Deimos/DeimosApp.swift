bd3e13e559a6d40f9e9eb9d3e9d6589ada4975c8
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-04-17 08:56:41 -0600

    Initial Commit

---
+++ Deimos/DeimosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeimosApp.swift
+//  Deimos
+//
+//  Created by Oscar Roberto Valdes Rodriguez on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DeimosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
